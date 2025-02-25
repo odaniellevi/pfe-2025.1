52f32e8c7365448880ce6810bce3f6e4b57208d3
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2025-02-25 00:32:37 +0000

    botao-numero-aleatorio

--- ex01/app/page.js
+++ ex01/app/page.js
@@ -1,15 +1,43 @@
+'use client';
+
+import { useState } from "react";
+
 function Texto1() {
-  return <p>Um outro parágrafo de texto</p>
+  return <p>O PALMEIRAS NÃO TEM MUNDIAL</p>
+}
+
+function MyButton() {
+  return (
+    <button>
+      Eu sou um botão
+    </button>
+  );
+}
+
+function gerarNumAleat() {
+  return Math.floor(Math.random() * 100) + 1;
 }
 
 export default function Home() {
-  const aula = 2
+  const [hide, setHide] = useState(false);
+  const [numAleat, setNumAleat] = useState(0);
+  const aula = 2;
   return (
     <div>
-      <h1>Olá Turma!!!</h1>
-      <p>Essa é a nossa {aula}ª aula de React</p>
-      <p>Um outro parágrafo de texto</p>
-      <Texto1 />
+      <h1>Olá Turma!!!!</h1>
+      <p>Essa é a nossa {aula}ª aula de React!</p>
+      <hr />
+      <button onClick={() => setNumAleat(gerarNumAleat())}>
+        Aletatório: {numAleat}
+      </button>
+      <hr />
+      <button onClick={() => setHide(!hide)}>{hide ? "Show" : "Hide"}</button>
+      <hr />
+      {!hide && (
+        <>
+        <Texto1 />
+        </>
+      )}
     </div>
   )
 }
