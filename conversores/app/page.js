74b4e1e3c02a06f4f8c65b5da8a26a7a079e8f91
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2025-04-24 21:10:14 +0000

    conversor concluido com alterações

--- conversores/app/page.js
+++ conversores/app/page.js
@@ -9,11 +9,11 @@ export default function Home() {
       <p className = "mb-6 max-w-xl">
         Este é um aplicativo de conversões rápidas e fáceis.
       </p>
-      <div className = "flex flex-col gap-4">
-        <Link href = "/sobre" className = "text-blue-600 hover:underline">Sobre</Link>
-        <Link href = "/dolar" className = "text-blue-600 hover:underline">Conversor de Dólar</Link>
-        <Link href = "/temperatura" className = "text-blue-600 hover:underline">Conversor de Temperatura</Link>
-        <Link href = "/comprimento" className = "text-blue-600 hover:underline">Conversor de Comprimento</Link>
+      <div className = "flex flex-col gap-4 w-full max-w-xs">
+        <Link href = "/sobre" className = "text-blue-600 text-white font-bold py-2 px-4 rounded-xl hover:bg-blue-700 transition-all duration-200 transform hover:scale-105 text-center">Sobre</Link>
+        <Link href = "/dolar" className = "text-blue-600 text-white font-bold py-2 px-4 rounded-xl hover:bg-blue-700 transition-all duration-200 transform hover:scale-105 text-center">Conversor de Dólar</Link>
+        <Link href = "/temperatura" className = "text-blue-600 text-white font-bold py-2 px-4 rounded-xl hover:bg-blue-700 transition-all duration-200 transform hover:scale-105 text-center">Conversor de Temperatura</Link>
+        <Link href = "/comprimento" className = "text-blue-600 text-white font-bold py-2 px-4 rounded-xl hover:bg-blue-700 transition-all duration-200 transform hover:scale-105 text-center">Conversor de Comprimento</Link>
         </div>
     </main>
   )
